55247eda454ca69814a5ad04b8d0914af2e39dd3
Igor Knop <igorknop@gmail.com>
2018-08-29 20:14:26 -0300

    Adiciona extras no intent para professor

--- app/src/main/java/br/ufjf/dcc196/exemplo02/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/exemplo02/MainActivity.java
@@ -5,21 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnProfessor;
+    private Button btnProfessor;
+    private EditText edtNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtNome = findViewById(R.id.edt_nome);
 
         btnProfessor = findViewById(R.id.btn_professor);
         btnProfessor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ProfessorActivity.class);
+                intent.putExtra("nome", edtNome.getText().toString());
                 startActivity(intent);
             }
         });
