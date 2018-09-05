57fb1bc0a2ee26e7602685b3f8a729cb28d44104
Igor Knop <igorknop@gmail.com>
2018-09-05 19:54:25 -0300

    Altera intent para oferecer retorno para main

--- app/src/main/java/br/ufjf/dcc196/exemplo02/ProfessorActivity.java
+++ app/src/main/java/br/ufjf/dcc196/exemplo02/ProfessorActivity.java
@@ -1,23 +1,46 @@
 package br.ufjf.dcc196.exemplo02;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ProfessorActivity extends AppCompatActivity {
     private TextView txtProfMensagem;
+    private Button btnProfConfirmar;
+    private EditText edtSiape;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professor);
-        txtProfMensagem = findViewById(R.id.txt_prof_mensagem);
+        txtProfMensagem = (TextView) findViewById(R.id.txt_prof_mensagem);
+        btnProfConfirmar = (Button) findViewById(R.id.btn_prof_confirmar);
+        edtSiape = (EditText) findViewById(R.id.edt_siape);
+
+
         Bundle extras = getIntent().getExtras();
         if(extras != null){
             String nome = extras.getString(MainActivity.PESSOA_NOME);
             txtProfMensagem.setText("Olá prof. "+nome+"!");
         }
+
+        btnProfConfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultado = new Intent();
+                resultado.putExtra(MainActivity.PROF_SIAPE, edtSiape.getText().toString());
+                setResult(Activity.RESULT_OK, resultado);
+                finish();
+            }
+        });
+
+
         Toast.makeText(getApplicationContext(), "prof onCreate()",Toast.LENGTH_SHORT).show();
     }
 
